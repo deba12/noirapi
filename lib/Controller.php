c1b577e55aef7cea9d2b402f58e10ee1d6e8bcfc
int986@gmail.com <int986@gmail.com>
2021-04-16 21:26:19 +0300

    cleanup

--- lib/Controller.php
+++ lib/Controller.php
@@ -7,48 +7,48 @@ use stdClass;
 class Controller {
 
     /** @var stdClass $request */
-	public $request;
+    public $request;
     /** @var array $server */
     public $server;
     /** @var Model $model */
     public $model;
-	/** @var Response $response */
-	public $response;
-	/** @var View $view */
-	public $view;
+    /** @var Response $response */
+    public $response;
+    /** @var View $view */
+    public $view;
 
     /**
      * Controller constructor.
      * @param stdClass $request
      * @param array $server
      */
-	public function __construct(stdClass $request, array $server) {
+    public function __construct(stdClass $request, array $server) {
 
-		$this->request = $request;
-		$this->server = $server;
+        $this->request = $request;
+        $this->server = $server;
 
-		if(defined('DB')) {
-			$model = 'app\\models\\' . self::getClassName(get_class($this));
-			if(class_exists($model)) {
-				$this->model = new $model($this->request);
-			} else {
-				$this->model = new Model();
-			}
-		}
+        if(defined('DB')) {
+            $model = 'app\\models\\' . self::getClassName(get_class($this));
+            if(class_exists($model)) {
+                $this->model = new $model($this->request);
+            } else {
+                $this->model = new Model();
+            }
+        }
 
 
-		$this->response = new Response();
+        $this->response = new Response();
 
-	}
+    }
 
-	/**
-	 * @param string $class
-	 * @return string
-	 */
-	public static function getClassName(string $class):string {
-		$path = explode('\\', $class);
-		return array_pop($path);
-	}
+    /**
+     * @param string $class
+     * @return string
+     */
+    public static function getClassName(string $class):string {
+        $path = explode('\\', $class);
+        return array_pop($path);
+    }
 
     /**
      * @param string $location
@@ -58,13 +58,13 @@ class Controller {
      * @noinspection PhpUnused
      * @noinspection UnknownInspectionInspection
      */
-	public function forward(string $location, int $status = 302): Response {
+    public function forward(string $location, int $status = 302): Response {
 
-		if($status !== 302 && $status !== 301) {
-			throw new UnableToForwardException('Unable to forward with status code: ' . $status);
-		}
+        if($status !== 302 && $status !== 301) {
+            throw new UnableToForwardException('Unable to forward with status code: ' . $status);
+        }
 
-		return $this->response->withStatus($status)->withLocation($location);
-	}
+        return $this->response->withStatus($status)->withLocation($location);
+    }
 
 }
