d21f0cc9a16292370ce85d5045f8d4537ec8d23f
int986@gmail.com <int986@gmail.com>
2021-09-15 22:57:50 +0300

    add random username

--- helpers/Users.php
+++ helpers/Users.php
@@ -317,6 +317,22 @@ class Users {
     }
 
     /**
+     * @param int $len
+     * @return string
+     * @throws Exception
+     */
+    public static function random(int $len = 16): string {
+        $chars = '23456789abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+        $res = '';
+
+        for($i = 1; $i <= $len; $i++) {
+            $res .= $chars[random_int(1, strlen($chars)-1)];
+        }
+
+       return $res;
+    }
+
+    /**
      * @param string $password
      * @return false|string|null
      * @noinspection PhpUnusedPrivateMethodInspection
