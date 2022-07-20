a750fbcda853e0738a409a1fdd65101938cb364b
int986@gmail.com <int986@gmail.com>
2022-07-20 11:09:40 +0300

    don't register panel twice

--- lib/TracyExtras.php
+++ lib/TracyExtras.php
@@ -11,20 +11,34 @@ use Tracy\Debugger;
 
 class TracyExtras {
 
+    private static array $panels = [];
+
     public function __construct() {
 
         /** @noinspection PhpUndefinedClassInspection */
-        if(class_exists(GitVersionPanel::class)) {
+        if(class_exists(GitVersionPanel::class) && !isset(self::$panels[ 'git' ])) {
+            self::$panels['git'] = true;
             Debugger::getBar()->addPanel(GitVersionPanel::createDefault());
         }
 
         foreach(\noirapi\lib\Model::tracyGetPdo() as $driver => $pdo) {
-            $panel = new PDOBarPanel($pdo);
-            $panel->title = $driver;
-            Debugger::getBar()->addPanel($panel);
+            if(!isset(self::$panels[ $driver ])) {
+                self::$panels[ $driver ] = true;
+
+                $panel = new PDOBarPanel($pdo);
+                $panel->title = $driver;
+                Debugger::getBar()->addPanel($panel);
+            }
         }
 
-        Debugger::getBar()->addPanel(new CurlBarPanel());
+        if(!isset(self::$panels[ 'curl' ])) {
+            self::$panels[ 'curl' ] = true;
+
+            $panel = new CurlBarPanel();
+            $panel->title = 'Curl';
+
+            Debugger::getBar()->addPanel($panel);
+        }
 
     }
 
