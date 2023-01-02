d521b2269949bf30f1ab763f5f9284cd2cfa15aa
int986@gmail.com <int986@gmail.com>
2023-01-03 00:02:33 +0200

    View:: pager cleanup

--- helpers/Macros.php
+++ helpers/Macros.php
@@ -47,6 +47,7 @@ class Macros extends Extension {
      * @param Tag $tag
      * @return Node
      * @noinspection PhpUnused
+     * @noinspection PhpUnusedParameterInspection
      */
     public function pager(Tag $tag): Node {
 
@@ -56,16 +57,29 @@ class Macros extends Extension {
             $file = ROOT . '/noirapi/templates/pager.latte';
         }
 
-        $subject = $tag->parser->parseUnquotedStringOrExpression();
-
         return new AuxiliaryNode(
-            fn (PrintContext $context) => $context->format('
-                $latte = new Latte\Engine;
-                $latte->setTempDirectory(dirname(__DIR__) . \'/temp\');
-                $latte->addExtension(new Latte\Essential\RawPhpExtension);
-                echo $latte->renderToString(\'%raw\', [%node]);',
-                $file,
-                $subject
+            fn(PrintContext $context) => $context->format('
+                if(empty($pager)) {
+                    throw new RuntimeException("Pager is not setup");
+                }
+
+                if($pager->getPageCount() == 1) { $idxl = 0; $idxr = 0; }
+                else if($pager->getPageCount() < 5) {
+                    $idxl = round($pager->getPageCount() / $pager->getPage(), 0, PHP_ROUND_HALF_UP) + $pager->getPage();
+                    $idxr = $pager->getPageCount()-$pager->getPage();
+                }
+                else if($pager->getPage() == 1) { $idxl = 0; $idxr = 4; }
+                else if($pager->getPage() == 2) { $idxl = 1; $idxr = 3; }
+                else if($pager->getPage() == $pager->getLastPage()) { $idxl = 4; $idxr = 0; }
+                else if($pager->getPage() == $pager->getLastPage() -1 ) { $idxl = 3; $idxr = 1; }
+                else { $idxl = 2; $idxr = 2; }
+
+                $this->createTemplate(\'%raw\', [
+                    \'pager\' => $pager,
+                    \'idxl\' => $idxl,
+                    \'idxr\' => $idxr,
+                 ], \'include\')->renderToContentType(\'html\');',
+                $file
             )
         );
 
