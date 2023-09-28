543d9a4d74255fd46e6e068c53404d0fb33cb8ae
int986@gmail.com <int986@gmail.com>
2023-09-28 20:32:36 +0300

    cleanup

--- helpers/EasyTranslator.php
+++ helpers/EasyTranslator.php
@@ -6,7 +6,6 @@ namespace noirapi\helpers;
 use Nette\Neon\Exception;
 use Nette\Neon\Neon;
 use noirapi\interfaces\Translator;
-use RuntimeException;
 
 class EasyTranslator implements Translator
 {
@@ -28,7 +27,6 @@ class EasyTranslator implements Translator
         } else {
             self::$cache[$this->language] = [];
         }
-
     }
 
     /**
@@ -38,7 +36,6 @@ class EasyTranslator implements Translator
      * @return string
      */
     public function translate(string $message, ?string $key = null, ...$args): string {
-
         // Condition is like /en,
         if($message === '/') {
             return '/' . $this->language;
@@ -50,7 +47,6 @@ class EasyTranslator implements Translator
         }
 
         return $this->lookup(self::$cache[$this->language] ?? [], $message, $key, ...$args);
-
     }
 
     /**
@@ -61,7 +57,6 @@ class EasyTranslator implements Translator
      * @return string
      */
     private function lookup(array $translations, string $message, ?string $key = null, ...$args): string {
-
         $args = array_map(fn($arg) => $this->urlTranslate($arg), $args);
 
         if($key !== null) {
@@ -102,7 +97,6 @@ class EasyTranslator implements Translator
         }
 
         return str_contains($message, '%s') ? sprintf($message, ...$args) : $message;
-
     }
 
     /**
