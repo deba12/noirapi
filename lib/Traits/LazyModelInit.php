9b8673f9ce2d64aae41c29a03fae309b7b7a2569
int986@gmail.com <int986@gmail.com>
2024-10-17 00:33:03 +0300

    implement lazy model

---
+++ lib/Traits/LazyModelInit.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace noirapi\lib\Traits;
+
+use noirapi\lib\Attributes\LazyModel;
+use Nette\SmartObject;
+use ReflectionClass;
+
+trait LazyModelInit
+{
+
+    use SmartObject;
+
+    private ReflectionClass $reflection;
+
+    public function __construct()
+    {
+
+        $this->reflection = new ReflectionClass($this);
+
+        foreach($this->reflection->getProperties() as $property) {
+            // Sets Value to null if property has LazyModel Attribute
+            if($property->getAttributes(LazyModel::class)) {
+                $name = $property->getName();
+                unset($this->$name);
+            }
+        }
+
+    }
+
+
+    /**
+     * @param string $name
+     * @return void
+     */
+    public function __get(string $name)
+    {
+
+        foreach($this->reflection->getProperties() as $property) {
+            // Sets Value to null if property has LazyModel Attribute
+            if($property->name === $name  && $property->getAttributes(LazyModel::class)) {
+                $instance = $property->getAttributes(LazyModel::class)[0]->newInstance();
+                $args = [];
+                foreach($instance->args as $arg) {
+                    if(property_exists($this, $arg)) {
+                        $args[] = $this->$arg;
+                    }
+                }
+
+                return $instance->model->{$instance->method}(...$args);
+
+            }
+        }
+
+        return $this->$name;
+
+    }
+
+}
