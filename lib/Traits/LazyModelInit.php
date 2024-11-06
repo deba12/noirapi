6a3b35d19b36943dd985be8a22170d71259a0e16
int986@gmail.com <int986@gmail.com>
2024-11-06 19:54:14 +0200

    cleanup lazy model

--- lib/Traits/LazyModelInit.php
+++ lib/Traits/LazyModelInit.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 namespace noirapi\lib\Traits;
 
-use noirapi\lib\Attributes\LazyModel;
 use Nette\SmartObject;
+use noirapi\lib\Attributes\LazyModel;
 use ReflectionClass;
 
 trait LazyModelInit
@@ -19,9 +19,9 @@ trait LazyModelInit
 
         $this->reflection = new ReflectionClass($this);
 
-        foreach($this->reflection->getProperties() as $property) {
+        foreach ($this->reflection->getProperties() as $property) {
             // Sets Value to null if property has LazyModel Attribute
-            if($property->getAttributes(LazyModel::class)) {
+            if ($property->getAttributes(LazyModel::class)) {
                 $name = $property->getName();
                 unset($this->$name);
             }
@@ -37,18 +37,19 @@ trait LazyModelInit
     public function __get(string $name)
     {
 
-        foreach($this->reflection->getProperties() as $property) {
+        foreach ($this->reflection->getProperties() as $property) {
             // Sets Value to null if property has LazyModel Attribute
-            if($property->name === $name  && $property->getAttributes(LazyModel::class)) {
+            if ($property->name === $name && $property->getAttributes(LazyModel::class)) {
                 $instance = $property->getAttributes(LazyModel::class)[0]->newInstance();
                 $args = [];
 
-                foreach($instance->args as $arg) {
-                    if(property_exists($this, $arg)) {
-                        if($this->$arg === null) {
+                foreach ($instance->args as $arg) {
+                    if (property_exists($this, $arg)) {
+                        if ($this->$arg === null) {
                             return null;
                         }
                         $args[] = $this->$arg;
+
                         break;
                     }
                 }
