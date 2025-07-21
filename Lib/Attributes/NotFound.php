594711226a3e96ba32271b506ad5c88d6e1bb9d7
int986@gmail.com <int986@gmail.com>
2025-07-21 11:15:53 +0300

    make NotFound attribute repeatable

--- Lib/Attributes/NotFound.php
+++ Lib/Attributes/NotFound.php
@@ -7,7 +7,7 @@ namespace Noirapi\Lib\Attributes;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_METHOD)]
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class NotFound
 {
     public string $message;
