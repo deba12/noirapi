b855fbb36c09cafff84fa45db71ce9f4219a9786
int986@gmail.com <int986@gmail.com>
2026-04-29 14:09:35 +0300

    add StringTemplate

---
+++ Helpers/StringTemplate.php
@@ -0,0 +1,32 @@
+<?php
+/** @noinspection PhpUnused */
+
+declare(strict_types=1);
+
+namespace Noirapi\Helpers;
+
+use Latte;
+use noirapi\Config;
+use Noirapi\Lib\View\FilterExtension;
+
+class StringTemplate
+{
+
+    private Latte\Engine $latte;
+
+    public function __construct(private readonly string $template)
+    {
+
+        $this->latte = new Latte\Engine;
+        $this->latte->setTempDirectory(Config::getTemp());
+        $this->latte->addExtension(new FilterExtension());
+        $this->latte->setLoader(new Latte\Loaders\StringLoader());
+
+    }
+
+    public function print(array $params = []): string
+    {
+        return $this->latte->renderToString($this->template, $params);
+    }
+
+}
