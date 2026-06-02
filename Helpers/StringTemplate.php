22f689b014a328a93373b21e2b094d02e18156fa
int986@gmail.com <int986@gmail.com>
2026-06-02 15:02:36 +0300

    cleanup

--- Helpers/StringTemplate.php
+++ Helpers/StringTemplate.php
@@ -1,4 +1,5 @@
 <?php
+
 /** @noinspection PhpUnused */
 
 declare(strict_types=1);
@@ -11,22 +12,18 @@ use Noirapi\Lib\View\FilterExtension;
 
 class StringTemplate
 {
-
     private Latte\Engine $latte;
 
     public function __construct(private readonly string $template)
     {
-
-        $this->latte = new Latte\Engine;
+        $this->latte = new Latte\Engine();
         $this->latte->setTempDirectory(Config::getTemp());
         $this->latte->addExtension(new FilterExtension());
         $this->latte->setLoader(new Latte\Loaders\StringLoader());
-
     }
 
     public function print(array $params = []): string
     {
         return $this->latte->renderToString($this->template, $params);
     }
-
 }
